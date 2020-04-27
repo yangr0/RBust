dee9f9f836ce1377a2787ffaff066bbf8c585b73
iinc0gnit0 <iinc0gni0t@pm.me>
2020-04-27 16:48:06 -0500

    0.2 - Added reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello World");
+// Dependencies
+use reqwest;
+
+
+
+// Main
+fn main() -> Result<String, reqwest::Error>> {
+    let mut response = reqwest::get("http://httpbin.org/get")?;
+    let mut body = String::new();
+    response.read_to_string(&mut body)?;
+
+    println!("Status: {}", response.status());
+
+    Ok(());
 }
