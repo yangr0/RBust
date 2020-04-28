92bdda48a8ae1b048d7e03c34028445f23173396
iinc0gnit0 <iinc0gni0t@pm.me>
2020-04-28 18:51:23 -0500

    vv0.2 - Added reqwest

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ use reqwest;
 
 // Main
 fn main() -> Result<String, reqwest::Error>> {
-    let mut response = reqwest::get("http://httpbin.org/get")?;
+    let mut response = reqwest::get("https://rust-lang.org")?;
     let mut body = String::new();
     response.read_to_string(&mut body)?;
 
-    println!("Status: {}", response.status());
+    if response.status.trim() == "200" {
+        println!!("Status {}", response.status());
+    }
 
     Ok(());
 }
