0372dd5b30c04c1f87429bfe2097a2b409a589ba
iinc0gnit0 <iinc0gni0t@pm.me>
2020-05-06 14:46:02 -0500

    v0.0.6 - Working on reading from file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,17 @@
-# v0.1
-- Initial Start
+# v0.0.6
+- Working on reading from file
 
-# v0.2
-- Added reqwest
+# v0.0.5
+- Made the request
 
-# v0.3
-- Added command line args
-
-# v0.4
+# v0.0.4
 - Working on handling the request
 
-# v0.5
-- Made the request
+# v0.0.3
+- Added command line args
+
+# v0.0.2
+- Added reqwest
+
+# v0.0.1
+- Initial Start
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 // Created by inc0gnit0 / skript0r
-// Version v0.3
-// 4/29/20
+// Version v0.0.6
+// 5/5/20
 
 
 
 // Dependencies
-use reqwest;
+use reqwest; // 0.10.4
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+
+
+// Read from file
+fn readfile() {
+    let filepath = "default.txt";
+    let file = File::open(filepath).unwrap();
+    let reader = BufReader::new(file);
+
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        let url = "https://github.com/";
+        let url = url.to_owned() + &line; // Add the URL and wordlist together
+        println!("{}", url);
+    }
+}
 
 
 
 // Make Request
 #[tokio::main]
 async fn request() -> Result<String, reqwest::Error> {
-    let response = reqwest::get("https://rustlang.org")
+    let response = reqwest::get("https://github.com/")
         .await?;
+    // Intrepreting the status code
     let result =
         if response.status() == 404 {
             "Page Not Found".to_owned()
@@ -27,10 +46,12 @@ async fn request() -> Result<String, reqwest::Error> {
 
 // Main
 fn main() {
+    // Handling errors in request()
     let result = match request() {
         Ok(response) => response,
         Err(_) => "Request failed".to_owned()
     };
 
     println!("{}", result);
+    readfile();
 }
